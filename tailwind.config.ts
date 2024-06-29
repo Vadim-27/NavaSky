a6d98d580cc2371e7c4d9fed2721817137955045
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-06-29 21:52:17 +0300

    added fonts and colors for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#161616',
+                    dark: '#262626',
+                },
+                blue: {
+                    100: '#84c0ef',
+                },
+                violet: {
+                    100: '#b5cafc',
+                },
+                gray: {
+                    100: '#eeeeee',
+                    200: '#c8c8c8',
+                    300: 'a8a8a8',
+                },
+            },
+            fontFamily: {
+                gilroy: '"Gilroy", sans-serif',
+                nunito: '"Nunito Sans", sans-serif',
+                gotham: '"Gotham Book", sans-serif',
+            },
+            screens: {
+                desktop: '1600px',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
