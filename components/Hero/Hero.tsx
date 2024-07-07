ebdda9b356d3e6e1db0637f33840e081f52af0fe
ledavto <ser_ega@ukr.net>
2024-07-07 23:14:47 +0300

    position hero element+add lineSVG

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,32 +1,34 @@
 import scss from './Hero.module.scss';
+import { Box, Container, Link, List, ListItem, Stack, Typography } from '@mui/material';
+import Image from 'next/image';
 
 const Hero = () => {
     return (
-        <section className="relative h-screen overflow-hidden bg-gradient-to-r from-purple-600 to-blue-600 text-white">
-            {/* <div class="absolute inset-0">
-                    <img
-                        src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxjb2RlfGVufDB8MHx8fDE2OTQwOTg0MTZ8MA&ixlib=rb-4.0.3&q=80&w=1080"
-                        alt="Background Image"
-                        class="h-full w-full object-cover object-center"
-                    />
-                    <div class="absolute inset-0 bg-black opacity-50"></div>
-                </div> */}
+        <Box component="section" className={scss.hero}>
+            <Stack direction="column" alignItems="center">
+                <Typography component="h1" className={scss.titleHero}>
+                    NavaSky
+                </Typography>
+                <Image
+                    className={scss.lineHero}
+                    src="/icons/lineHero.svg"
+                    width={380}
+                    height={43}
+                    alt="lineHero"
+                />
 
-            <div className="relative z-10 flex h-full flex-col items-center justify-center text-center">
-                <h1 className="mb-4 text-5xl font-bold leading-tight"></h1>
-                NavaSky
-                <p className="mb-8 text-lg text-gray-300">
-                    We see ourselves as partners of our clients, launching their websites into the
-                    orbit of success through full-funnel marketing approach
-                </p>
-                <a
-                    href="#"
-                    className="transform rounded-full bg-blue-400 px-6 py-2 text-lg font-semibold text-gray-900 transition duration-300 ease-in-out hover:scale-105 hover:bg-yellow-300 hover:shadow-lg"
-                >
-                    Let’s talk
-                </a>
-            </div>
-        </section>
+                <Typography component="p" className={scss.textHero}>
+                    We help brands scale by bringing date driven decisions and creativity together
+                    for a full-funnel marketing approach
+                </Typography>
+                <div>
+                    <Link href="#" target="_blank" rel="" className={scss.btnLetsTalk}>
+                        <div className={scss.btnHeroText}>Let’s talk</div>
+                        <Image src="/icons/circle-arrow.svg" width={33} height={33} alt="Narrov" />
+                    </Link>
+                </div>
+            </Stack>
+        </Box>
     );
 };
 
