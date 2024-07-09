bc5df24128c60d53cf7999e6d5f8070930685f42
ledavto <ser_ega@ukr.net>
2024-07-09 16:48:23 +0300

    edit font

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -6,32 +6,31 @@ import Mountain from './Mountain';
 const Hero = () => {
     return (
         <Box component="section" className={scss.hero}>
-            <Stack direction="column" alignItems="center">
-                <div className={scss.titleHero}>
-                    <Typography component="h1" className={scss.titleHeroText}>
-                        NavaSky
-                    </Typography>
-                    <Image
-                        className={scss.lineHero}
-                        src="/icons/lineHero.svg"
-                        width={380}
-                        height={43}
-                        alt="lineHero"
-                    />
-                </div>
-
-                <Typography component="p" className={scss.textHero}>
-                    We help brands scale by bringing date driven decisions and creativity together
-                    for a full-funnel marketing approach
+            <div className={scss.titleHero}>
+                <Typography component="h1" className={scss.titleHeroText}>
+                    NavaSky
                 </Typography>
-                <div>
-                    <Link href="#" target="_blank" rel="" className={scss.btnLetsTalk}>
-                        <div className={scss.btnHeroText}>Let’s talk</div>
-                        <Image src="/icons/circle-arrow.svg" width={33} height={33} alt="Narrov" />
-                    </Link>
-                </div>
-                <Mountain />
-            </Stack>
+                <Image
+                    className={scss.lineHero}
+                    src="/icons/lineHero.svg"
+                    width={380}
+                    height={43}
+                    alt="lineHero"
+                />
+            </div>
+
+            <Typography component="p" className={scss.textHero}>
+                We help brands scale by bringing date driven decisions and creativity together for a
+                full-funnel marketing approach
+            </Typography>
+            <div>
+                <Link href="#" target="_blank" rel="" className={scss.btnLetsTalk}>
+                    <div className={scss.btnHeroText}>Let’s talk</div>
+                    <Image src="/icons/circle-arrow.svg" width={33} height={33} alt="Narrov" />
+                </Link>
+            </div>
+
+            <Mountain />
         </Box>
     );
 };
