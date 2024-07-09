bf4b31b783c1f4e5a3fdc9d20fa83714b493f4ca
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-07-09 15:57:28 +0300

    added localization for navigation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,14 @@
 'use client';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 const NotFound = () => {
     return (
         <html>
             <body className="flex h-screen flex-col items-center justify-center">
                 <h1 className="mb-4 text-4xl font-semibold">Something went wrong!</h1>
-                <Link href="/" className="rounded-xl bg-blue-100 px-8 py-4 text-white">
+                {/* <Link href="/" className="rounded-xl bg-blue-100 px-8 py-4 text-white">
                     Return to Home page
-                </Link>
+                </Link> */}
             </body>
         </html>
     );
--- components/Header/Navigation/Navigation.tsx
+++ components/Header/Navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Link from 'next/link';
 import { Box } from '@mui/material';
 import scss from './Navigation.module.scss';
+import { useTranslations } from 'next-intl';
 
 interface NavigationPagesProps {
     label: string;
@@ -13,11 +14,13 @@ interface NavigationProps {
 }
 
 const Navigation: FC<NavigationProps> = ({ pages }) => {
+    const t = useTranslations('Header');
+
     return (
         <Box component="div" className={scss.navigationList}>
             {pages.map(({ href, label }) => (
                 <Link href={href} key={label} className={scss.navigationLink}>
-                    {label}
+                    {t(label)}
                 </Link>
             ))}
         </Box>
